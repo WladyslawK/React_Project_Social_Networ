2d816708f44339858de0af8552b24245a1ddbe85
WladyslawK <vladkurr7890@gmail.com>
2022-08-01 22:56:17 +0200

    Css modules created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import s from "./Header.module.css"
 
 export const Header = () => {
     return (
     <>
-        <header className="header">
-            <img src ="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/NBC_logo.svg/2000px-NBC_logo.svg.png"/>
+        <header className={s.header}>
+            <img className={s.img} src ="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/NBC_logo.svg/2000px-NBC_logo.svg.png"/>
         </header>
     </>
   );
